f294017f161929bfa1cb72e22f4fdfee5786a466
Paulo Ruiz <paulo.ruiz@thoughtworks.com>
2021-06-15 12:17:34 -0400

    add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,63 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Button,
+  makeStyles,
+  Tabs,
+  Tab } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+  // root: {
+  //   flexGrow: 1,
+  // },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  // title: {
+  //   flexGrow: 1,
+  // },
+}))
 
 const Navbar = (props) => {
+  const classes = useStyles()
+  const history = useHistory()
   const { routes } = props
 
-  return (<nav data-testid='Navbar'>
-    <div>
-      <ul>
+  // return (<nav data-testid='Navbar'>
+  //   <div>
+  //     <ul>
+
+  //     </ul>
+  //   </div>
+  // </nav>)
+
+  return (<AppBar position='static' data-testid='Navbar'>
+    <Toolbar>
+      <IconButton
+        edge='start'
+        className={classes.menuButton}
+        color='inherit'
+        aria-label='menu'>
+        <MenuIcon />
+      </IconButton>
+      <Typography variant='h6' className={classes.title}>
+          News
+      </Typography>
+      <Tabs>
         {routes.map((route, indexRoute) => (
-          <li
-            key={`route-item-${indexRoute}`}
-          >
-            <Link to={route.to}>{route.title}</Link>
-          </li>))}
-      </ul>
-    </div>
-  </nav>)
+          <Tab key={indexRoute} onClick={() => {
+            history.push(route.to)
+          }} label={route.title}>
+          </Tab>))}
+      </Tabs>
+      <Button color='inherit'>Login</Button>
+    </Toolbar>
+  </AppBar>)
 }
 
 Navbar.propTypes = {
